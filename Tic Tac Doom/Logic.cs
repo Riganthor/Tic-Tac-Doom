295ddca0474e2a4f9e988e897993c0a69c8bed18
riganthor <johannssven@gmail.com>
2025-04-01 15:34:06 +0200

    added game logic and ability to add X's and O's for 2 player.

--- Tic Tac Doom/Logic.cs
+++ Tic Tac Doom/Logic.cs
@@ -1,26 +1,84 @@
 ﻿using System;
 
-public class TicTacDoom
-{
-    private const int GRIDSIZE = 3;
-    private int[,] grid;
-
-    public TicTacDoom()
+namespace TicTacDoomConsole
+{ 
+    public class GameLogic
     {
-        grid = new int[GRIDSIZE, GRIDSIZE];
-    }
+        private char[,] grid = new char[3, 3];
+        private bool isPlayerX = true;
 
-    public void DisplayGrid()
-    {
-        Console.WriteLine("+---+---+---+");
-        for (int i = 0; i < GRIDSIZE; i++)
+        public GameLogic()
         {
-            for (int j = 0; j < GRIDSIZE; j++)
+            InitializeGrid();
+        }
+
+        public void InitializeGrid()
+        {
+            for (int i = 0; i < 3; i++)
             {
-                Console.Write("|   ");
+                for (int j = 0; j < 3; j++)
+                {
+                    grid[i, j] = ' ';
+                }
             }
-            Console.WriteLine("|");
-            Console.WriteLine("+---+---+---+");
+        }
+
+        public char[,] GetGrid()
+        {
+            return grid;
+        }
+
+        public bool MakeMove(int row, int col)
+        {
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && grid[row, col] == ' ')
+            {
+                grid[row, col] = isPlayerX ? 'X' : 'O';
+                isPlayerX = !isPlayerX;
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckForWinner()
+        {
+            // Check rows, columns, and diagonals for a winner
+            for (int i = 0; i < 3; i++)
+            {
+                if (grid[i, 0] == grid[i, 1] && grid[i, 1] == grid[i, 2] && grid[i, 0] != ' ')
+                {
+                    Console.WriteLine($"Player {grid[i, 0]} wins!");
+                    return true;
+                }
+                if (grid[0, i] == grid[1, i] && grid[1, i] == grid[2, i] && grid[0, i] != ' ')
+                {
+                    Console.WriteLine($"Player {grid[0, i]} wins!");
+                    return true;
+                }
+            }
+            if (grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2] && grid[0, 0] != ' ')
+            {
+                Console.WriteLine($"Player {grid[0, 0]} wins!");
+                return true;
+            }
+            if (grid[0, 2] == grid[1, 1] && grid[1, 1] == grid[2, 0] && grid[0, 2] != ' ')
+            {
+                Console.WriteLine($"Player {grid[0, 2]} wins!");
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckForTie()
+        {
+            foreach (char cell in grid)
+            {
+                if (cell == ' ')
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine("It's a tie!");
+            return true;
         }
     }
 }
--- Tic Tac Doom/Program.cs
+++ Tic Tac Doom/Program.cs
@@ -1,11 +1,74 @@
-﻿namespace Tic_Tac_Doom
+﻿using System;
+using TicTacDoomConsole;
+
+namespace TicTacDoomConsole
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            TicTacDoom game = new TicTacDoom();
-            game.DisplayGrid();
+            GameLogic game = new GameLogic();
+            bool gameOver = false;
+
+            while (!gameOver)
+            {
+                DisplayGrid(game.GetGrid());
+                gameOver = MakeMove(game);
+                if (!gameOver)
+                {
+                    gameOver = game.CheckForWinner();
+                }
+                if (!gameOver)
+                {
+                    gameOver = game.CheckForTie();
+                }
+            }
+
+            DisplayGrid(game.GetGrid());
+            Console.WriteLine("Game Over!");
+        }
+
+        private static void DisplayGrid(char[,] grid)
+        {
+            Console.Clear();
+            Console.WriteLine("Tic Tac Toe");
+            Console.WriteLine("-----------");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(" " + grid[i, 0] + " | " + grid[i, 1] + " | " + grid[i, 2]);
+                if (i < 2)
+                {
+                    Console.WriteLine("---+---+---");
+                }
+            }
+            Console.WriteLine("-----------");
+        }
+
+        private static bool MakeMove(GameLogic game)
+        {
+            int row, col;
+            do
+            {
+                Console.Write($"Player {(game.GetGrid()[0, 0] == ' ' ? 'X' : 'O')}, enter your move (row and column): ");
+                string input = Console.ReadLine();
+                string[] coordinates = input.Split(' ');
+                if (coordinates.Length == 2 && int.TryParse(coordinates[0], out row) && int.TryParse(coordinates[1], out col))
+                {
+                    if (game.MakeMove(row, col))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid move. Try again.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
+            } while (true);
+            return false;
         }
     }
 }
