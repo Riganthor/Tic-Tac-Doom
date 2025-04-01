8a05ac24eae607266ff0098d303a5d83a8e8cfab
riganthor <johannssven@gmail.com>
2025-04-01 15:06:27 +0200

    changed the logic code to show an empty 3*3 grid

--- Tic Tac Doom/Logic.cs
+++ Tic Tac Doom/Logic.cs
@@ -4,42 +4,23 @@ public class TicTacDoom
 {
     private const int GRIDSIZE = 3;
     private int[,] grid;
-    private Random rng;
 
     public TicTacDoom()
     {
-        rng = new Random();
         grid = new int[GRIDSIZE, GRIDSIZE];
-        FillGrid();
-    }
-
-    private void FillGrid()
-    {
-        for (int i = 0; i < GRIDSIZE; i++)
-        {
-            for (int j = 0; j < GRIDSIZE; j++)
-            {
-                grid[i, j] = rng.Next(0, 3); // Random numbers 0, 1, or 2
-            }
-        }
     }
 
     public void DisplayGrid()
     {
         Console.WriteLine("+---+---+---+");
-        for (int i = 0; i < grid.GetLength(0); i++)
+        for (int i = 0; i < GRIDSIZE; i++)
         {
-            for (int j = 0; j < grid.GetLength(1); j++)
+            for (int j = 0; j < GRIDSIZE; j++)
             {
-                Console.Write($"| {grid[i, j]} ");
+                Console.Write("|   ");
             }
             Console.WriteLine("|");
             Console.WriteLine("+---+---+---+");
         }
-    }
-    public static void Main(string[] args)
-    {
-        TicTacDoom game = new TicTacDoom();
-        game.DisplayGrid();
     }
 }
