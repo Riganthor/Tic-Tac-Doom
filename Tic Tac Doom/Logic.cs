aba6922346a24c3cb811cbd0289ba5ff1aea2bfc
riganthor <johannssven@gmail.com>
2025-04-01 14:56:42 +0200

    added the logic to create the grid, right now with error

--- Tic Tac Doom/Logic.cs
+++ Tic Tac Doom/Logic.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Tic_Tac_Doom
+public class TicTacDoom
 {
-    internal class Logic
+    private const int GRIDSIZE = 3;
+    private int[,] grid;
+    private Random rng;
+
+    public TicTacDoom()
     {
+        rng = new Random();
+        grid = new int[GRIDSIZE, GRIDSIZE];
+        FillGrid();
+    }
+
+    private void FillGrid()
+    {
+        for (int i = 0; i < GRIDSIZE; i++)
+        {
+            for (int j = 0; j < GRIDSIZE; j++)
+            {
+                grid[i, j] = rng.Next(0, 3); // Random numbers 0, 1, or 2
+            }
+        }
+    }
+
+    public void DisplayGrid()
+    {
+        Console.WriteLine("+---+---+---+");
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                Console.Write($"| {grid[i, j]} ");
+            }
+            Console.WriteLine("|");
+            Console.WriteLine("+---+---+---+");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        TicTacDoom game = new TicTacDoom();
+        game.DisplayGrid();
     }
 }
