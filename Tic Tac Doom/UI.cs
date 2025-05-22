374b799eb7fb4eb6b670aa79d1c713b3ca7fed72
SvenJohanns <johannssven@gmail.com>
2025-05-22 11:03:17 +0200

    fixed the makemove by getting the grid in

--- Tic Tac Doom/UI.cs
+++ Tic Tac Doom/UI.cs
@@ -7,7 +7,7 @@ using TicTacDoomConsole;
 
 namespace Tic_Tac_Doom
 {
-    internal class UI
+    public class UI
     {
 
         public void DisplayGrid(char[,] grid)
@@ -26,17 +26,17 @@ namespace Tic_Tac_Doom
             Console.WriteLine("-----------");
         }
 
-        public bool MakeMove(GameLogic game)
+        public void MakeMove(GameLogic game, char[,] grid)
         {
             int row, col;
             do
             {
-                Console.Write($"Player {(game.GetGrid()[0, 0] == ' ' ? 'X' : 'O')}, enter your move (row and column): ");
+                Console.Write($"Player {(game.GetGrid(grid)[0, 0] == ' ' ? 'X' : 'O')}, enter your move (row and column): ");
                 string input = Console.ReadLine();
                 string[] coordinates = input.Split(' ');
                 if (coordinates.Length == 2 && int.TryParse(coordinates[0], out row) && int.TryParse(coordinates[1], out col))
                 {
-                    if (game.MakeMove(row, col))
+                    if (game.MakeMove(row, col, grid))
                     {
                         break;
                     }
